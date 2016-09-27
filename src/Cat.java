28dd2272afaa5e71b2fad2b95f0c6148b21fe32b
anlogan <anlogan@mines.edu>
2016-09-27 14:49:59 -0600

    Changed messsage to help me and made head size 200

--- src/Cat.java
+++ src/Cat.java
@@ -5,7 +5,7 @@ import java.awt.geom.*;
 public class Cat {
 	// drawing constants are private - noone needs to know what we're doing
 	// pick a head dimension
-	private static final int HEAD_DIMENSION = 100;
+	private static final int HEAD_DIMENSION = 200;
 	// eyes will be about 1/4 from top of head and 1/4 from left
 	private static final int EYE_Y = HEAD_DIMENSION/4;
 	private static final int EYE_X = HEAD_DIMENSION/4;
@@ -44,6 +44,6 @@ public class Cat {
 		g2.setColor(Color.black);
 		// Meow text appears below cat head, +10 places below 
 		// so it doesn't overlap the drawing
-		g2.drawString("Bang it out", catX, catY+HEAD_DIMENSION+10);	
+		g2.drawString("Help me", catX, catY+HEAD_DIMENSION+10);	
 	}
 }
